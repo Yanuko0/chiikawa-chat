240af50593cfad2938b55f39be1b14e72f185864
Yanuko0 <https://github.com/Yanuko0>
2024-11-26 13:37:57 +0800

    準備部署到 GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/chiikawa-chat/',
+  build:{
+    outDir: 'docs'
+  },
   plugins: [react()],
 })
